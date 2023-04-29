11016a7ab5edc3802ef62885a80c111c3958937e
Leger DJIBA <leger.djiba@outlook.fr>
2023-04-29 20:55:52 +0000

    First Commit

---
+++ Partie3/Program.cs
@@ -0,0 +1,13 @@
+﻿int PrixProduit = 1250;
+int QuantiteProduit = 100;
+const int tva = 18;
+
+int MontantHorsTaxe = PrixProduit * QuantiteProduit;
+int MontantTva = MontantHorsTaxe * tva /100;
+int MontantTtc = MontantHorsTaxe + MontantTva;
+
+Console.WriteLine($"Présentation du produit");
+Console.WriteLine($"Prix d'achat : {PrixProduit}, quantité achetée : {QuantiteProduit}, Tva : {tva}");
+Console.WriteLine($"Le montant hors taxe est : {MontantHorsTaxe}");
+Console.WriteLine($"Le montant de la TVA est : {MontantTva}");
+Console.WriteLine($"Le montant de la toute taxe comprise est : {MontantTtc}");
